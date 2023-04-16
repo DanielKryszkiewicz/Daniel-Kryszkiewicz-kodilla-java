8880f875d0b172e3e5a81241a7e0090af1b77e60
DanielKryszkiewicz <kryszkiewicz.daniel1@gmail.com>
2023-04-16 14:36:19 +0200

    6.2 added Calculator test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,35 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(5,5);
+        int subtractResult = calculator.subtract(5, 5);
+
+        if(addResult == 10) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        if(subtractResult == 0){
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
-
