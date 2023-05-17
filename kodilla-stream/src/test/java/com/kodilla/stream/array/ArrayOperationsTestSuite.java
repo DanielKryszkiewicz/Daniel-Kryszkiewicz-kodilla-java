8ee75062910fe9ec603df175baa0c0e765d014fb
DanielKryszkiewicz <kryszkiewicz.daniel1@gmail.com>
2023-05-17 13:29:49 +0200

    7.5 IntStream

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,19 @@
+package com.kodilla.stream.array;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ArrayOperationsTestSuite {
+
+    @Test
+   void testGetAverage(){
+        //Given
+        int[] numbers = {1,6,4,7,9,};
+        double expAvg = 5.4;
+        //When
+        double actualAverage = ArrayOperations.getAverage(numbers);
+        //Then
+        Assertions.assertEquals(expAvg,actualAverage);
+
+    }
+}
