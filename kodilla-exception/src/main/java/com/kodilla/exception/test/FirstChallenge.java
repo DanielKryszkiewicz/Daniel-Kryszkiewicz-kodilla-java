9289b013a829c362cf693b41e4579773a961ab84
DanielKryszkiewicz <kryszkiewicz.daniel1@gmail.com>
2023-05-18 17:17:59 +0200

    8.2 trycatchfinally

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -2,29 +2,27 @@ package com.kodilla.exception.test;
 
 public class FirstChallenge {
     public double divide(double a, double b) throws ArithmeticException {
-        try {
-            if (b == 0) {
-                throw new ArithmeticException();
-            }
+
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+        public static void main(String[] args) {
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+
+
+        try { double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
         } catch (ArithmeticException e ) {
             System.out.println("Something went wrong... "+ e);
         } finally {
             System.out.println("I am gonna be here.. always!");
         }
-        return a / b;
-    }
-
-    /**
-     * This main can throw an ArithmeticException!!!
-     * @param args
-     */
-    public static void main(String[] args) {
-
-        FirstChallenge firstChallenge = new FirstChallenge();
-
-        double result = firstChallenge.divide(3, 0);
-
-        System.out.println(result);
-
     }
 }
+
+
