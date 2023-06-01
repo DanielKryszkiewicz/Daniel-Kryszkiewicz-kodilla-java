f9c75c59a4c715c32faa4357f098f820a576bacf
DanielKryszkiewicz <kryszkiewicz.daniel1@gmail.com>
2023-06-01 21:49:57 +0200

    8.3 reformat

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,11 +1,11 @@
 package com.kodilla.exception.test;
 
 public class ExceptionHandling {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 
         SecondChallenge secondChallenge = new SecondChallenge();
         try {
-        secondChallenge.probablyIWillThrowException(2,2);
+            secondChallenge.probablyIWillThrowException(2, 2);
         } catch (Exception e) {
             System.out.println("An exception occurred");
         } finally {
--- kodilla-exception/src/test/java/com/kodilla/exception/SecondChallengeTestSuite.java
+++ kodilla-exception/src/test/java/com/kodilla/exception/SecondChallengeTestSuite.java
@@ -5,14 +5,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SecondChallengeTestSuite {
-@Test
-    public void testProbablyWillThrowException(){
-    //Given
-    SecondChallenge secondChallenge = new SecondChallenge();
-    //When & Then
-    Assertions.assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(0,1));
-    Assertions.assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(2,1.5));
-    Assertions.assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(2,0));
-    Assertions.assertDoesNotThrow(() -> secondChallenge.probablyIWillThrowException(1.5,1));
+    @Test
+    public void testProbablyWillThrowException() {
+        //Given
+        SecondChallenge secondChallenge = new SecondChallenge();
+        //When & Then
+        Assertions.assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(0, 1));
+        Assertions.assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(2, 1.5));
+        Assertions.assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(2, 0));
+        Assertions.assertDoesNotThrow(() -> secondChallenge.probablyIWillThrowException(1.5, 1));
     }
 }
