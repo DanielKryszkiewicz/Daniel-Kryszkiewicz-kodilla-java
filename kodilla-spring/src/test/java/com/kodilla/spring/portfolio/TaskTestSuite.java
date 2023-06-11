45557cc2c50efaa5f0a9fb4635a585dad8b8a3e2
DanielKryszkiewicz <kryszkiewicz.daniel1@gmail.com>
2023-06-11 18:58:14 +0200

    14.2 test splitted

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/TaskTestSuite.java
@@ -0,0 +1,35 @@
+package com.kodilla.spring.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class TaskTestSuite {
+
+    @Autowired
+    private Board board;
+
+    @Test
+    public void testTaskAdd() {
+        //Given
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+        Board board = context.getBean(Board.class);
+
+        // When
+
+        board.getToDoList().getTasks().add("To Do Task");
+        board.getInProgressList().getTasks().add("In Progress Task");
+        board.getDoneList().getTasks().add("Done Task");
+
+        // Then
+        assertEquals("To Do Task", board.getToDoList().getTasks().get(0));
+        assertEquals("In Progress Task", board.getInProgressList().getTasks().get(0));
+        assertEquals("Done Task", board.getDoneList().getTasks().get(0));
+    }
+}
